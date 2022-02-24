375c6bea62aaa1d782ddaf715ecc66ae06d3948e
OscarDeLeon19 <oscar99deleon19@gmail.com>
2022-02-24 15:47:53 -0600

    Agregamos la demostracion de graficas de Barra a la ejecucion

--- Practica1Compiladores1/app/src/main/java/practica/main/EjecucionGraficas.kt
+++ Practica1Compiladores1/app/src/main/java/practica/main/EjecucionGraficas.kt
@@ -1,12 +1,19 @@
 package practica.main
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
+import androidx.core.view.marginBottom
+import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.LegendEntry
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import graficos.Barra
 import graficos.Pie
@@ -28,9 +35,12 @@ class EjecucionGraficas : AppCompatActivity() {
 
         var layoutL = findViewById(R.id.linealLayout) as LinearLayout
 
-        var parametrosLayout = LinearLayout.LayoutParams(1000,800)
-        println(LinearLayout.LayoutParams.MATCH_PARENT)
-        println(LinearLayout.LayoutParams.WRAP_CONTENT)
+        var parametrosLayout = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.MATCH_PARENT)
+        parametrosLayout.bottomMargin = 100
+        parametrosLayout.gravity = 1
+
+        parametrosLayout.width = 600
+        parametrosLayout.height=600
         var colors = ArrayList<Int>()
         for (color in ColorTemplate.MATERIAL_COLORS) {
             colors.add(color)
@@ -42,7 +52,42 @@ class EjecucionGraficas : AppCompatActivity() {
         for (i  in  lista.indices) {
             if (lista[i] is Barra) {
 
+                var graficaBarra = lista[i] as Barra
+                var grafica:BarChart = BarChart(this)
+                var listaEtiquetas = graficaBarra.ejeX
+                var listaValores = graficaBarra.ejeY
+                var listaUniones = graficaBarra.unir
+                grafica.setLayoutParams(parametrosLayout)
+                grafica.description.setText(graficaBarra.titulo)
+                grafica.description.setTextSize(10f)
 
+                var entradas = ArrayList<BarEntry>()
+                var etiquetas = ArrayList<String>()
+                var i = 0
+                var pos = 0
+                while (i < listaUniones.size) {
+                    etiquetas.add(listaEtiquetas[listaUniones[i]])
+                    var entrada = BarEntry(pos.toFloat(),listaValores[listaUniones[i+1]].toFloat())
+                    entradas.add(entrada)
+                    pos++
+                    i+=2
+                }
+                var xasis = grafica.xAxis
+                xasis.setGranularityEnabled(true)
+                xasis.setLabelRotationAngle(45f)
+                xasis.setPosition(XAxis.XAxisPosition.BOTTOM)
+                var index = IndexAxisValueFormatter(etiquetas)
+                xasis.setValueFormatter(index)
+
+                var dataSet = BarDataSet(entradas, graficaBarra.titulo)
+
+                var data = BarData(dataSet)
+                data.setBarWidth(0.4f)
+                dataSet.setColors(colors)
+                grafica.setData(data)
+                grafica.setFitBars(true)
+
+                layoutL.addView(grafica)
             } else if (lista[i] is Pie) {
                 var graficaPie = lista[i] as Pie
                 var grafica:PieChart = PieChart(this)
@@ -88,7 +133,8 @@ class EjecucionGraficas : AppCompatActivity() {
                 dataSet.setValues(dataValores)
                 dataSet.setLabel(graficaPie.titulo)
                 dataSet.setColors(colors)
-                dataSet.setValueTextSize(16f)
+                dataSet.setValueTextSize(10f)
+                dataSet.setValueTextColor(Color.BLACK)
 
                 var dataPie = PieData(dataSet)
 
@@ -97,15 +143,15 @@ class EjecucionGraficas : AppCompatActivity() {
 
 
                 grafica.description.setEnabled(false)
+                grafica.setEntryLabelTextSize(10f)
+                grafica.setEntryLabelColor(Color.BLACK)
                 grafica.setCenterText(graficaPie.titulo)
                 dataSet.setFormLineWidth(4F)
 
-
                 layoutL.addView(grafica)
             }
+
         }
-
-
 
 
     }
--- Practica1Compiladores1/app/src/main/java/practica/main/EjecutarGraficas.kt
+++ Practica1Compiladores1/app/src/main/java/practica/main/EjecutarGraficas.kt
@@ -8,10 +8,13 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.PieData
-import com.github.mikephil.charting.data.PieDataSet
-import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.LegendEntry
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.google.android.material.transition.MaterialSharedAxis
 import graficos.Barra
 import graficos.Grafica
 import graficos.Pie
@@ -97,7 +100,55 @@ class EjecutarGraficas : AppCompatActivity() {
 
         for (i  in  lista.indices){
             if (lista[i] is Barra){
+
+                var graficaBarra = lista[i] as Barra
+                val registro = LayoutInflater.from(this).inflate(R.layout.item_grafica1, null, false)
+                val itemGrafica1 = registro.findViewById<View>(R.id.itemGraficaBar) as BarChart
+
+                var listaEtiquetas = graficaBarra.ejeX
+                var listaValores = graficaBarra.ejeY
+                var listaUniones = graficaBarra.unir
+
+                itemGrafica1.description.setText(graficaBarra.titulo)
+                itemGrafica1.description.setTextSize(16f)
+
+                var entradas = ArrayList<BarEntry>()
+
+                var leyenda = itemGrafica1.legend
+                leyenda.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER)
+
+                var etiquetas = ArrayList<String>()
+
+
+                var i = 0
+                var pos = 0
+                while (i < listaUniones.size) {
+
+                    etiquetas.add(listaEtiquetas[listaUniones[i]])
+                    var entrada = BarEntry(pos.toFloat(),listaValores[listaUniones[i+1]].toFloat())
+                    entradas.add(entrada)
+                    pos++
+                    i+=2
+                }
+
+                var xasis = itemGrafica1.xAxis
+                xasis.setGranularityEnabled(true)
+                xasis.setPosition(XAxis.XAxisPosition.TOP)
+                var index = IndexAxisValueFormatter(etiquetas)
+                xasis.setValueFormatter(index)
+
+                var dataSet = BarDataSet(entradas, "Grafica Barras")
+
+                var data = BarData(dataSet)
+                data.setBarWidth(0.4f)
+                dataSet.setColors(colors)
+                itemGrafica1.setData(data)
+                itemGrafica1.setFitBars(true)
+                tablaLayout?.addView(registro)
+
+
             }else if (lista[i] is Pie){
+                /*
                 println("Se encontro graficaPie")
                 var graficaPie = lista[i] as Pie
                 val registro = LayoutInflater.from(this).inflate(R.layout.item_grafica1, null, false)
@@ -144,6 +195,9 @@ class EjecutarGraficas : AppCompatActivity() {
                 itemGrafica1.setCenterText(graficaPie.titulo)
                 dataSet.setFormLineWidth(4F)
                 tablaLayout?.addView(registro)
+
+
+                 */
             }
         }
 
